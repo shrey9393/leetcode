bea554e84cac61a8086fdc174b00694d4c457928
shrey9393 <shreyanshagrawal9393@gmail.com>
2022-12-29 20:47:09 +0530

    day29

---
+++ day29_decSingleThread.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    vector<int> getOrder(vector<vector<int>> &tasks)
+    {
+        for (int i = 0; i < tasks.size(); i++)
+        {
+            tasks[i].push_back(i);
+        }
+        sort(tasks.begin(), tasks.end());
+        priority_queue <vector<int>, vector<vector<int>>, greater<vector<int>>> priorque;
+        vector<int> a;
+        int maxEnqTime = 0;
+        int i = 0;
+        while (a.size() < tasks.size())
+        {
+            if (i < tasks.size() && priorque.empty())
+            {
+                maxEnqTime = max(maxEnqTime, tasks[i][0]);
+            }
+            while (i < tasks.size() && maxEnqTime >= tasks[i][0])
+            {
+                priorque.push({tasks[i][1], tasks[i][2]});
+                i++;
+            }
+            auto x = priorque.top();
+            priorque.pop();
+            maxEnqTime = min(1000000000, x[0] + maxEnqTime);
+            a.push_back(x[1]);
+        }
+        return a;
+    }
+};
