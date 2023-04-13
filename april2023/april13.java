a97a70f5c1e34e49daf2753ab8da16a758f06d2b
shrey9393 <shreyanshagrawal9393@gmail.com>
2023-04-13 10:43:34 +0530

    day 13

---
+++ april2023/april13.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public static boolean validateStackSequences(int[] a, int[] b) {
+        if (a.length != b.length) {
+            return false;
+        }
+        Stack<Integer> st = new Stack<>();
+        int j = 0;
+        for (int x : a) {
+            st.push(x);
+            if (!st.isEmpty() && st.peek() == b[j]) {
+                st.pop();
+                j++;
+            }
+        }
+        return st.isEmpty();
+        // return true;
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 3, 4, 5 };
+        int b[] = { 4, 5, 3, 2, 1 };
+        System.out.println(validateStackSequences(a, b));
+
+    }
+}
