1b61f4a6441900f24116c49c887d6300101a5282
shrey9393 <shreyanshagrawal9393@gmail.com>
2023-04-20 20:05:19 +0530

    day141

---
+++ ExtraProbs/leetCode287FindtheDuplicateNumber.java
@@ -0,0 +1,26 @@
+class leetCode287FindtheDuplicateNumber {
+    public static int findDuplicate(int[] nums) {
+        int fast = nums[0];
+        int slow = nums[0];
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        // fast = nums[0];
+        // while (slow != fast) {
+        //     slow = nums[slow];
+        //     fast = nums[fast];
+        // }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 4, 2, 2 };
+        System.out.println(findDuplicate(arr));
+    }
+}
