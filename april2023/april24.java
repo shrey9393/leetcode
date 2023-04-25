c46de03ef8203c0690e4f9a45424db3d7e12dbf1
shrey9393 <shreyanshagrawal9393@gmail.com>
2023-04-25 17:01:21 +0530

    day146

--- april2023/april24.java
+++ april2023/april24.java
@@ -1,32 +1,10 @@
-import java.util.*;
-
-class april24 {
-    public static int lastStoneWeight(int[] stones) {
-        if (stones.length == 1) {
-            return stones[0];
-        }
-        ArrayList<Integer> ts = new ArrayList<>();
-        for (int i : stones) {
-            ts.add(i);
-        }
-        if (ts.size() == 1) {
-            return 0;
-        }
-        while (ts.size() != 0) {
-            int y = ts.get(ts.size() - 1);
-            ts.remove(ts.size() - 1);
-            int x = ts.get(ts.size() - 1);
-            ts.remove(ts.size() - 1);
-            // ts.remove(x);
-            if (y - x != 0) {
-                ts.add(y - x);
-            }
-        }
-        return ts.get(0);
-    }
-
-    public static void main(String[] args) {
-        int arr[] = { 2, 2 };
-        System.out.println(lastStoneWeight(arr));
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+     PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b - a);
+        for (int a : stones)
+            pq.offer(a);
+        while (pq.size() > 1)
+            pq.offer(pq.poll() - pq.poll());
+        return pq.poll();
     }
 }
