4228d0b99a316cfc9035e196df2bfbc752294fff
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-07-11 22:38:59 +0530

    Time: 57 ms (64.81%), Space: 41.8 MB (72.32%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+    const res = new Array(arr.length);
+    for(let i =0 ;i<arr.length;i++){
+        res[i] = fn(arr[i],i);
+    }
+    return res;
+};
