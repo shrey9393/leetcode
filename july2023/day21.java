91563de45a2a958e69e83ce3e3f5fe88f061dd70
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-07-21 12:02:52 +0530

    Create day21.java

---
+++ july2023/day21.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] lengths = new int[n]; 
+        int[] counts = new int[n]; 
+        Arrays.fill(lengths, 1);
+        Arrays.fill(counts, 1);
+
+        int maxLength = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (lengths[j] + 1 > lengths[i]) {
+                        lengths[i] = lengths[j] + 1;
+                        counts[i] = counts[j];
+                    } else if (lengths[j] + 1 == lengths[i]) {
+                        counts[i] += counts[j];
+                    }
+                }
+            }
+            maxLength = Math.max(maxLength, lengths[i]);
+        }
+
+        int numberOfLIS = 0;
+        for (int i = 0; i < n; i++) {
+            if (lengths[i] == maxLength) {
+                numberOfLIS += counts[i];
+            }
+        }
+
+        return numberOfLIS;
+    }
+}
