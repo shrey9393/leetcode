f59c81a9eb2f0cb1bc0f75e17c5ae72413b328e6
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-08-27 21:43:19 +0530

    day 270

--- New folder/Solution.java
+++ New folder/Solution.java
@@ -1,31 +1,30 @@
+
+/**
+ * Complete the implementation of an autocorrect function. Given a search query string the
+function should return all words which are anagrams.
+Given 2 arrays, wordslnl and queries[ql, for each query, return an array of the strings that are
+anagrams, sorted alphabetically ascending.
+Note: An anagram is any string that can be formed by rearranging the letters of a string.
+Example
+words = ["duel", "speed", "dule", "cars"),
+queries = ["spede", "deul"].
+The only anagram of "spede" is "speed".
+Both "duel" and "dule" are anagrams of "deul".
+Return [("speed"l, ["duel", "dule"JJ.
+Function Description
+Complete the function getSearchResu/ts in the editor below.
+getSearchResults takes the following arguments:
+string words[n]: the list of words to search
+string queries[ql: the words to search for
+Returns
+the results for each search query
+ */
+// reffer 
+
 import java.util.*;
 
 class Solution {
-    public static boolean dfs(char c1[], char c2[], char c3[], int i, int j, int k, boolean dp[][]) {
-        if (dp[i][j]) {
-            return false;
-        }
-        if (k == c3.length) {
-            return true;
-        }
-        boolean valid = i < c1.length && c1[i] == c3[k] && dfs(c1, c2, c3, i + 1, j, k + 1, dp)
-                || j < c2.length && c2[j] == c3[k] && dfs(c1, c2, c3, i, j + 1, k + 1, dp);
-        if (!valid) {
-            dp[i][j] = true;
-        }
-        return valid;
-    }
-
-    public static boolean isInterleave(String s1, String s2, String s3) {
-        char c1[] = s1.toCharArray(), c2[] = s2.toCharArray(), c3[] = s3.toCharArray();
-        int m = s1.length(), n = s2.length();
-        if (m + n != s3.length()) {
-            return false;
-        }
-        boolean dp[][] = new boolean[m + 1][n + 1];
-        return dfs(c1, c2, c3, 0, 0, 0, dp);
-
-    }
+    public static
 
     public static void main(String[] args) {
         System.out.println(isInterleave("aabc", "abad", "aabcabad"));
