aaea637817b17a7d1cb61ebaaddbccb1eacb41a2
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-08-30 15:14:09 +0530

    273

--- New folder/Solution.java
+++ New folder/Solution.java
@@ -1,32 +1,20 @@
-
-/**
- * Complete the implementation of an autocorrect function. Given a search query string the
-function should return all words which are anagrams.
-Given 2 arrays, wordslnl and queries[ql, for each query, return an array of the strings that are
-anagrams, sorted alphabetically ascending.
-Note: An anagram is any string that can be formed by rearranging the letters of a string.
-Example
-words = ["duel", "speed", "dule", "cars"),
-queries = ["spede", "deul"].
-The only anagram of "spede" is "speed".
-Both "duel" and "dule" are anagrams of "deul".
-Return [("speed"l, ["duel", "dule"JJ.
-Function Description
-Complete the function getSearchResu/ts in the editor below.
-getSearchResults takes the following arguments:
-string words[n]: the list of words to search
-string queries[ql: the words to search for
-Returns
-the results for each search query
- */
-// reffer 
-
-import java.util.*;
-
 class Solution {
-    public static
+    public static int maximizeSum(int[] nums, int k) {
+        int sum = 0;
+        int i = 0;
+        for (i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (; i < nums.length; i++) {
+            sum += (nums[i] - nums[i - k]);
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
 
     public static void main(String[] args) {
-        System.out.println(isInterleave("aabc", "abad", "aabcabad"));
+        int arr[] = { 1, 2, 3, 4, 5 };
+        System.out.println(maximizeSum(arr, 3));
     }
 }
