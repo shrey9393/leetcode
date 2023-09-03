66597e1e35aac11f03fcd16b4ae2cbcbd38ba895
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-09-03 10:15:57 +0530

    277

--- New folder/Solution.java
+++ New folder/Solution.java
@@ -1,31 +1,20 @@
 class Solution {
-
-    public int threeSumClosest(int[] nums, int target) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        int closestSum = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n - 2; i++) {
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
-                    closestSum = sum;
-                }
-
-                if (sum < target) {
-                    left++;
-                } else if (sum > target) {
-                    right--;
-                } else {
-                    return closestSum;
-                }
+    public int furthestDistanceFromOrigin(String moves) {
+        int c1 = 0, c2 = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'R') {
+                c1--;
+            } else {
+                c1++;
             }
         }
-
-        return closestSum;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'L') {
+                c2--;
+            } else {
+                c2++;
+            }
+        }
+        return Math.max(c1, c2);
     }
-
 }
