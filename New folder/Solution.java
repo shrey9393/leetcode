9ccb7eaca23490f63f4c3f6242c7fb90340b31e6
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-09-07 21:11:44 +0530

    day281

--- New folder/Solution.java
+++ New folder/Solution.java
@@ -1,11 +1,12 @@
+import java.util.HashMap;
+
 class Solution {
-    public int totalHammingDistance(int[] nums) {
-        int ans = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                ans += Integer.bitCount(nums[i] ^ nums[j]);
-            }
-        }
-        return ans;
+    public HashMap<String, Integer> hm = new HashMap<>();
+
+    public void createMap(){
+        hm.put(1, "")
+    }
+    public String countAndSay(int n) {
+        
     }
 }
