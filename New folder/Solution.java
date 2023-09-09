9a4cbfa12ac388fa04cc835812588da3efd5c08f
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-09-09 19:19:39 +0530

    data

--- New folder/Solution.java
+++ New folder/Solution.java
@@ -1,12 +1,36 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
-    public HashMap<String, Integer> hm = new HashMap<>();
+    public List<Integer> mostVisited(int n, int[] rounds) {
+        int[] arr = new int[n + 1];
+        int max = 0;
 
-    public void createMap(){
-        hm.put(1, "")
+        for (int i = 0; i < rounds.length - 1; i++) {
+            int st = rounds[i];
+            int ed = rounds[i + 1];
+            while (st != ed) {
+                arr[st] = arr[st] + 1;
+                max = Math.max(max, arr[st]);
+                st = (st % n) + 1;
+            }
+        }
+
+        arr[rounds[rounds.length - 1]]++;
+        max = Math.max(max, arr[rounds[rounds.length - 1]]);
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (arr[i] == max) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
     }
-    public String countAndSay(int n) {
-        
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 1, 2 };
+        Solution s = new Solution();
+        System.out.println(s.mostVisited(4, arr)); // Output: [1, 2]
     }
 }
