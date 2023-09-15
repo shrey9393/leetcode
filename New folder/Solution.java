63007cb19d90886c533771e1a09e8ada544d7a0b
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-09-15 20:30:40 +0530

    day289

--- New folder/Solution.java
+++ New folder/Solution.java
@@ -1,43 +1,21 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
-    public List<String> findItinerary(List<List<String>> tickets) {
-        // Create a map to store the graph of airports and their destinations
-        HashMap<String, PriorityQueue<String>> graph = new HashMap<>();
-
-        // Build the graph
-        for (List<String> ticket : tickets) {
-            String from = ticket.get(0);
-            String to = ticket.get(1);
-            graph.computeIfAbsent(from, k -> new PriorityQueue<>()).add(to);
+    public String freqAlphabets(String s) {
+        HashMap<String, Character> hm = new HashMap<>();
+        char c = 'a';
+        for (int j = 1; j <= 26; j++) {
+            hm.put("" + j, c++);
         }
-
-        List<String> itinerary = new ArrayList<>();
-        dfs("JFK", graph, itinerary);
-
-        // Reverse the result since we start from JFK
-        Collections.reverse(itinerary);
-        return itinerary;
-    }
-
-    private void dfs(String airport, HashMap<String, PriorityQueue<String>> graph, List<String> itinerary) {
-        PriorityQueue<String> destinations = graph.get(airport);
-        while (destinations != null && !destinations.isEmpty()) {
-            String nextAirport = destinations.poll();
-            dfs(nextAirport, graph, itinerary);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 2 < s.length() && s.charAt(i + 2) == '#') {
+                sb.append(hm.get(s.substring(i, i + 2)));
+                i += 2;
+            } else {
+                sb.append(s.charAt(i));
+            }
         }
-        itinerary.add(airport);
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        List<List<String>> tickets = new ArrayList<>();
-        tickets.add(Arrays.asList("MUC", "LHR"));
-        tickets.add(Arrays.asList("JFK", "MUC"));
-        tickets.add(Arrays.asList("SFO", "SJC"));
-        tickets.add(Arrays.asList("LHR", "SFO"));
-
-        List<String> itinerary = solution.findItinerary(tickets);
-        System.out.println(itinerary);
+        return sb.toString();
     }
 }
