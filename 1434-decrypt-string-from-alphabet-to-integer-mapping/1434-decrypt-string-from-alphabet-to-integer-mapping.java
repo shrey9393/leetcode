1b26442257694b57c40ca6b5ccbca96aa41be78e
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-09-15 18:30:07 +0530

    Time: 3 ms (32.89%), Space: 40.9 MB (40.6%) - LeetHub

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public String freqAlphabets(String s) {
+        HashMap<String, Character> hm = new HashMap<>();
+        char c = 'a';
+        for (int j = 1; j <= 26; j++) {
+            hm.put("" + j, c++);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 2 < s.length() && s.charAt(i + 2) == '#') {
+                sb.append(hm.get(s.substring(i, i + 2)));
+                i += 2;
+            } else {
+                sb.append(hm.get(""+s.charAt(i)));
+            }
+        }
+        return sb.toString();
+    }
+}
