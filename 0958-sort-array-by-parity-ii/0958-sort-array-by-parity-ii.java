f135797030ffa1251f0cfdd553c50cf452f51730
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-11-01 15:18:15 +0530

    Time: 3 ms (46.01%), Space: 44.9 MB (74.67%) - LeetHub

---
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int i=0,j=1;
+		
+        while(i<nums.length && j<nums.length){
+            if(nums[i]%2==0){
+                i+=2;
+            } 
+            else{
+                if(nums[j]%2!=0) {
+                    j+=2;
+                }
+                else{
+                    int temp = nums[i];
+                    nums[i]=nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
