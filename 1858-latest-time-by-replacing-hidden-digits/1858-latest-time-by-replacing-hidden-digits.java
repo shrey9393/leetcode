0230ef26ded7c8f3dd9b463ab101d3ac3c4f4338
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-11-10 23:31:12 +0530

    Time: 5 ms (20%), Space: 40.6 MB (54.78%) - LeetHub

---
+++ 1858-latest-time-by-replacing-hidden-digits/1858-latest-time-by-replacing-hidden-digits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String maximumTime(String time) {
+         if (time.charAt(0) == '?') {
+            if (time.charAt(1) != '?' && time.charAt(1) - '0' > 3) {
+                time = time.substring(0, 0) + '1' + time.substring(1);
+            } else {
+                time = time.substring(0, 0) + '2' + time.substring(1);
+            }
+        }
+        if (time.charAt(1) == '?') {
+            if (time.charAt(0) == '1' || time.charAt(0) == '0') {
+                time = time.substring(0, 1) + '9' + time.substring(2);
+            } else {
+                time = time.substring(0, 1) + '3' + time.substring(2);
+            }
+        }
+        if (time.charAt(3) == '?') {
+            time = time.substring(0, 3) + '5' + time.substring(4);
+        }
+        if (time.charAt(4) == '?') {
+            time = time.substring(0, 4) + '9';
+        }
+        return time;
+    }
+}
