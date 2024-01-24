569703299d6473e9d601b6cc53a0b31298faca26
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2024-01-24 10:53:11 +0530

    data

---
+++ 2024/jan/janDay24_2024.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pseudoPalindromicPaths(TreeNode root) {
+        return countPseudoPalindromicPaths(root, 0);
+    }
+
+    private int countPseudoPalindromicPaths(TreeNode node, int path) {
+        if (node == null) {
+            return 0;
+        }
+        path ^= (1 << node.val);
+        if (node.left == null && node.right == null) {
+            return (path & (path - 1)) == 0 ? 1 : 0;
+        }
+        return countPseudoPalindromicPaths(node.left, path) + countPseudoPalindromicPaths(node.right, path);
+    }
+}
