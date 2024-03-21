ca2663ec0701f846df5e3ade950a3160cd4692c8
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2024-03-21 21:22:02 +0530

    Create marchday21.java

---
+++ 2024/march/marchday21.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode ne = null;
+        while(curr!= null){
+            ne = curr.next;
+            curr.next=prev;
+            prev = curr;
+            curr= ne;
+        }
+        ListNode x = prev;
+        return x; 
+
+
+    }
+}
